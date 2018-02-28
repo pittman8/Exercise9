3661864d7fc265ff32c81905abd28c36f4621eb1
pittman8 <pittman8@uw.edu>
2018-02-28 13:00:41 -0800

    rounded corners more using quadraticCurveTo()

--- roadSigns.js
+++ roadSigns.js
@@ -4,6 +4,8 @@ function draw() {
     var signs = document.querySelector("#signs");
     var ctx = signs.getContext('2d'); // pull the content out in 2D
 
+     //------------------SIGN #1: Do Not Enter Sign------------------------//
+    
     ctx.save(); // save previous display
 
     // Set drawing properties
@@ -31,19 +33,34 @@ function draw() {
     ctx.fillText("ENTER", 105, 195); // set font content, x-axis position, y-axis position
     ctx.restore(); // restore previous display state
     
+    //------------------SIGN #2: Stop Light Sign------------------------//
+    
     ctx.save(); // save coordinate system
-    // Draw second sign next to the first
-    ctx.translate(450, -375);
+    ctx.translate(450, -375); // draw second sign to the right of the first
     ctx.rotate(0.8); // rotate coordinate system
-    ctx.beginPath();
     
     // Set drawing properties
     ctx.lineWidth = "5"; // 10px width outline
     ctx.strokeStyle = "black"; // black outline
-    ctx.lineJoin = "round"; // round corners
     ctx.fillStyle = "gold"; // gold fill
     
-    ctx.rect(300, 300, 150, 150); // draw rectangle
+    // quadraticCurveTo() rounds rectangle corners (more rounded than lineJoin does)
+    var radius = 15; // corner curve radius
+    var height = 150;
+    var width = 150;
+    var x = 300; // start x-coordinate
+    var y = 305; // start y-coordinate
+    
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius); 
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height); 
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius); 
+    ctx.quadraticCurveTo(x, y, x + radius, y); 
     ctx.closePath();
     
     // Fill and outline sign
@@ -55,19 +72,19 @@ function draw() {
     
     // Draw black rectangle inside sign
     ctx.fillStyle = 'black'; // set fill color
-    ctx.fillRect(96, -461, 133, 45); // fill rectangle at starting x-axis, starting y-axis, width, and height
+    ctx.fillRect(99, -458, 133, 45); // fill rectangle at starting x-axis, starting y-axis, width, and height
 
     // Draw red circle inside black rectangle
     ctx.fillStyle = 'red'; // fill it red
     ctx.beginPath();
-    ctx.arc(122, -439, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
+    ctx.arc(125, -436, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
     ctx.closePath();
     ctx.fill();
     
     // Draw yellow circle inside black rectangle
     ctx.fillStyle = 'yellow'; // fill it yellow
     ctx.beginPath();
-    ctx.arc(162, -439, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
+    ctx.arc(166, -436, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
     ctx.closePath();
     ctx.fill();
 
@@ -75,7 +92,7 @@ function draw() {
     // Draw green circle inside black rectangle
     ctx.fillStyle = 'lime'; // fill it green
     ctx.beginPath();
-    ctx.arc(203, -439, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
+    ctx.arc(207, -436, 17, 0, 2 * Math.PI); // set middle of circle, radius, start and end angle
     ctx.closePath();
     ctx.fill();
 }
